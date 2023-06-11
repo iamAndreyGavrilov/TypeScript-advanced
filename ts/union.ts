2a31670b9fe8785da8b24134f410054bc17f99bf
AndreyG <boombardir20@gmail.com>
2023-06-11 16:08:53 +0500

    Union

---
+++ ts/union.ts
@@ -0,0 +1,29 @@
+// Union
+function logId(id: number | string) {
+  console.log(id)
+    if (typeof id === 'string') {
+        console.log(id.toUpperCase())
+    } else {
+        console.log(id)
+    }
+}
+logId(123)
+logId('123')
+// logId(true) // error
+
+function logError(err: string | string[]){
+    if (Array.isArray(err)) {
+        console.log(err.join(','))
+    } else {
+        console.log(err)
+    }
+}
+
+function logObj(obj: {a: number} | {b: number}) {
+    if ('a' in obj) {
+        console.log(obj.a)
+    } else {
+        console.log(obj.b)
+    }
+
+}
