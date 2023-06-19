0468796a4a0d2857bbb093f5d827074ca27435eb
AndreyG <boombardir20@gmail.com>
2023-06-19 20:08:34 +0500

    Void

---
+++ ts/Advanced Types/Void.ts
@@ -0,0 +1,20 @@
+// Void - это когда функция ничего не возвращает или нам не важно, что она возвращает
+
+function sayMyName(name: string): void {
+    console.log(name);
+}
+
+sayMyName('Heisenberg');
+
+const skills = ['JS', 'React', 'Node'];
+
+const user = {
+    name: 'Walter',
+    s:['TS']
+}
+
+skills.forEach((skill) : void => {
+    user.s.push(skill);
+});
+
+
