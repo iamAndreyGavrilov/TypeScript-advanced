01121dfe73285e40894405b38625576dee82f6e7
A G <a.gavrilof@zordoc.uz>
2023-06-24 14:02:16 +0500

    tsc --init

---
+++ ts/Advanced Types/Null.ts
@@ -0,0 +1,18 @@
+// Null - это тип, который представляет отсутствие значения.
+
+interface User234567 {
+    name: string;
+}
+
+function getUser(){
+    if (Math.random() > 0.5) {
+        return null
+    } else {
+        return { name: 'John' }
+    }
+}
+
+const user1 = getUser();
+if (user1){
+const name = user1.name;
+}
